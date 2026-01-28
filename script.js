5c7a2aa4223dd0aaaa5c01ee1e39ce9f1e7a8145
socialgraph <socialgraph@yandex.ru>
2026-01-28 14:42:47 +0300

    digits for textarea

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function addSection() {
   article.insertAdjacentHTML(
     "beforeend",
     `<section>
-  <textarea name="text" placeholder="${placeholder}" rows=${rows}></textarea>
+  <textarea name="text" placeholder="${placeholder}" rows=${rows} inputmode="text"></textarea>
   <button type="button">Добавить</button>
 </section>`,
   );
